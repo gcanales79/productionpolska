21d72b44684b97d745d2be00176b1c34793ab47c
Gustavo Canales <gustavo.canales@me.com>
2018-08-03 00:33:22 -0500

    Added cambiar in English

--- public/js/cambio.js
+++ public/js/cambio.js
@@ -11,28 +11,103 @@ $("#cambiar").on("click", function (event) {
     console.log("Serial Repetido Length " + serialRepetido.length);
     console.log("Nuevo Serial length " + nuevoSerial.length);
 
+    numeroParterepetido = serialRepetido.slice(0, 10);
+    numeroPartenuevo = nuevoSerial.slice(0, 10);
+
+    console.log("El NP repetido es: " + numeroParterepetido);
+    console.log("El NP nuevo es: " + numeroPartenuevo)
+
     if (serialRepetido.length === 22 && nuevoSerial.length === 22) {
-        $.get("/api/" + nuevoSerial, function (data) {
+        if (numeroParterepetido === numeroPartenuevo) {
+            $.get("/api/" + nuevoSerial, function (data) {
 
-            if (data) {
-                console.log(data);
-                $("#Respuesta").empty();
-                $("#Respuesta").text("La etiqueta nueva esta repetida por favor revisar");
-            }
-            else{
-                $.post("/api/cambioetiqueta",newSerial)
-                .then(function(){
+                if (data) {
+                    console.log(data);
                     $("#Respuesta").empty();
-                    $("#Respuesta").text("La etiqueta fue cambiada exitosamente");
-                });
+                    etiquetaRepetida();
+                }
+                else {
+                    $.post("/api/cambioetiqueta", newSerial)
+                        .then(function () {
+                            $("#Respuesta").empty();
+                            etiquetaExitosa();
+                        });
 
-                
-            }
-        });
+
+                }
+
+            });
+        }
+        else {
+            $("#Respuesta").empty();
+            numeroParteIncorrecto();
+        }
 
     }
     else {
-        $("#Respuesta").text("Alguno de los seriales no es de 22 digitos. Por favor corregir.")
+        $("#Respuesta").empty();
+        digitosIncorrectos();
+        
     }
 
 });
+
+
+$(document).on("click", "#polskaHtml", function (event) {
+    event.preventDefault();
+    window.location.href = "./polska.html";
+});
+
+function digitosIncorrectos(){
+    var newDiv = $("<div>")
+        var resultadoImagen = $("<img>")
+        resultadoImagen.attr("src", "./images/wrong.png");
+        resultadoImagen.attr("class", "resultadoImagen");
+        newDiv.text("One of the label has less that 22 digits. Please change");
+        newDiv.attr("class", "comentario");
+        $("#Respuesta").append(resultadoImagen);
+        $("#Respuesta").append(newDiv);
+        limpiarInput();
+}
+
+
+function numeroParteIncorrecto(){
+    var newDiv = $("<div>")
+        var resultadoImagen = $("<img>")
+        resultadoImagen.attr("src", "./images/wrong.png");
+        resultadoImagen.attr("class", "resultadoImagen");
+        newDiv.text("The labels don't have the same Part Number. Please change");
+        newDiv.attr("class", "comentario");
+        $("#Respuesta").append(resultadoImagen);
+        $("#Respuesta").append(newDiv);
+        limpiarInput();
+};
+
+function etiquetaRepetida(){
+    var newDiv = $("<div>")
+        var resultadoImagen = $("<img>")
+        resultadoImagen.attr("src", "./images/wrong.png");
+        resultadoImagen.attr("class", "resultadoImagen");
+        newDiv.text("The new label is already repeat. Don't use it and change the label");
+        newDiv.attr("class", "comentario");
+        $("#Respuesta").append(resultadoImagen);
+        $("#Respuesta").append(newDiv);
+        limpiarInput();
+};
+
+function etiquetaExitosa(){
+    var newDiv = $("<div>")
+    var resultadoImagen = $("<img>")
+    resultadoImagen.attr("src", "./images/good.png");
+    resultadoImagen.attr("class", "resultadoImagen");
+    newDiv.text("The label was changed succesfully");
+    newDiv.attr("class", "comentariobueno");
+    $("#Respuesta").append(resultadoImagen);
+    $("#Respuesta").append(newDiv);
+        limpiarInput();
+};
+
+function limpiarInput(){
+    $("#serialEtiquetarepetida").val("");
+    $("#serialEtiquetanueva").val("");
+}
--- public/js/polska.js
+++ public/js/polska.js
@@ -10,73 +10,104 @@ $("#submit").on("click", function (event) {
         $.get("/api/all/" + nuevoSerial, function (data) {
             console.log(data);
 
-            if (data.length>1) {
-                var newDiv = $("<div>")
-                var resultadoImagen = $("<img>")
-                resultadoImagen.attr("src", "./images/wrong.png");
-                resultadoImagen.attr("class", "resultadoImagen");
-                newDiv.text("The label is duplicated please change the label");
-                newDiv.attr("class", "comentario");
-                var newButton = $("<button>");
-                newButton.attr("class", "btn btn-primary");
-                newButton.attr("type", "submit");
-                newButton.attr("id", "cambioDeetiqueta");
-                newButton.text("Check Another Part");
-                $("#Resultado").append(resultadoImagen);
-                $("#Resultado").append(newDiv);
-                $("#Resultado").append(newButton);
-                $("#submit").prop("disabled", true);
+            if (data.length > 1) {
+                duplicatedLabel();
 
-                
-               /* $.post("/message", newSerial)
-                    .then(newSerial);*/
+
                 return;
             }
 
-            if(data.length===1){
-                var newDiv = $("<div>")
-                        var resultadoImagen = $("<img>")
-                        resultadoImagen.attr("src", "./images/good.png");
-                        resultadoImagen.attr("class", "resultadoImagen");
-                        newDiv.text("Good Label. The part is OK to ship");
-                        newDiv.attr("class", "comentariobueno");
-                        $("#serialEtiqueta").val("");
-                        $("#Resultado").append(resultadoImagen);
-                        $("#Resultado").append(newDiv);
+            if (data.length === 1) {
+                //console.log("Status de repetida " + data[0].repetida)
+                if (data[0].repetida && data[0].etiqueta_remplazada==null) {
+                    duplicatedLabel();
+                }
+
+
+                else {
+                    etiquetaCorrecta();
+                }
             }
 
-            if(data.length===0){
-                var newDiv = $("<div>")
-                        var resultadoImagen = $("<img>")
-                        resultadoImagen.attr("src", "./images/wrong.png");
-                        resultadoImagen.attr("class", "resultadoImagen");
-                        newDiv.text("The label is not on the database. Please change label");
-                        newDiv.attr("class", "comentario");
-                        $("#serialEtiqueta").val("");
-                        $("#Resultado").append(resultadoImagen);
-                        $("#Resultado").append(newDiv);
+            if (data.length === 0) {
+                etiquetaFaltante();
             }
-      
+
 
         });
 
 
     }
     else {
-        var newDiv = $("<div>")
-        var resultadoImagen = $("<img>")
-        resultadoImagen.attr("src", "./images/wrong.png");
-        resultadoImagen.attr("class", "resultadoImagen");
-        newDiv.text("The label needs to have 22 digits. ");
-        newDiv.attr("class", "comentario");
-        $("#serialEtiqueta").val("");
-        $("#Resultado").append(resultadoImagen);
-        $("#Resultado").append(newDiv);
+        serialIncorrecto();
 
     }
 })
 
 $(document).on("click", "#cambioDeetiqueta", function (event) {
     event.preventDefault();
-    window.location.href = "./polska.html";
+    window.location.href = "./cambiar.html";
 });
+
+function serialIncorrecto() {
+    var newDiv = $("<div>")
+    var resultadoImagen = $("<img>")
+    resultadoImagen.attr("src", "./images/wrong.png");
+    resultadoImagen.attr("class", "resultadoImagen");
+    newDiv.text("The label needs to have 22 digits. ");
+    newDiv.attr("class", "comentario");
+    $("#serialEtiqueta").val("");
+    $("#Resultado").append(resultadoImagen);
+    $("#Resultado").append(newDiv);
+}
+
+function duplicatedLabel() {
+    var newDiv = $("<div>")
+    var resultadoImagen = $("<img>")
+    resultadoImagen.attr("src", "./images/wrong.png");
+    resultadoImagen.attr("class", "resultadoImagen");
+    newDiv.text("The label is duplicated please change the label");
+    newDiv.attr("class", "comentario");
+    $("#serialEtiqueta").val("");
+    var newButton = $("<button>");
+    newButton.attr("class", "btn btn-primary");
+    newButton.attr("type", "submit");
+    newButton.attr("id", "cambioDeetiqueta");
+    newButton.text("Change Label");
+    $("#Resultado").append(resultadoImagen);
+    $("#Resultado").append(newDiv);
+    $("#Resultado").append(newButton);
+    $("#submit").prop("disabled", true);
+}
+
+function etiquetaFaltante() {
+    var newDiv = $("<div>")
+    var resultadoImagen = $("<img>")
+    resultadoImagen.attr("src", "./images/wrong.png");
+    resultadoImagen.attr("class", "resultadoImagen");
+    newDiv.text("The label is not on the database. Please change label");
+    newDiv.attr("class", "comentario");
+    $("#serialEtiqueta").val("");
+    var newButton = $("<button>");
+    newButton.attr("class", "btn btn-primary");
+    newButton.attr("type", "submit");
+    newButton.attr("id", "cambioDeetiqueta");
+    newButton.text("Change Label");
+    $("#Resultado").append(resultadoImagen);
+    $("#Resultado").append(newDiv);
+    $("#Resultado").append(newButton);
+    $("#submit").prop("disabled", true);
+}
+
+function etiquetaCorrecta() {
+    var newDiv = $("<div>")
+    var resultadoImagen = $("<img>")
+    resultadoImagen.attr("src", "./images/good.png");
+    resultadoImagen.attr("class", "resultadoImagen");
+    newDiv.text("Good Label. The part is OK to ship");
+    newDiv.attr("class", "comentariobueno");
+    $("#serialEtiqueta").val("");
+    $("#Resultado").append(resultadoImagen);
+    $("#Resultado").append(newDiv);
+}
+
