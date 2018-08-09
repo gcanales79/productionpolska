d2550e247932d5b1aa208bcc780f865726234d71
Gustavo Canales <gustavo.canales@me.com>
2018-08-08 21:41:49 -0500

    Added tablas view

---
+++ public/js/tabla.js
@@ -0,0 +1,9 @@
+$("#buscarRegistros").on("click", function (event) {
+    event.preventDefault();
+    var numeroDeregistros = $("#cantidadAbuscar").val().trim()
+    url = "/tabla/" + numeroDeregistros;
+    console.log("El url es " + url);
+    window.location.href = url;
+
+
+});
