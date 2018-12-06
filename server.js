4b5cafad53703818fa5c59bd8d9602cdf95f198a
Gustavo Canales <gustavo.canales@me.com>
2018-12-05 22:28:12 -0600

    added role view

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
-var cookieParser=require("cookie-parser")
+var cookieParser = require("cookie-parser")
 var exphbs = require("express-handlebars");
 var twilio = require("twilio");
 //var helpers = require('handlebars-helpers')();
@@ -10,9 +10,9 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 var db = require("./models");
-var axios=require("axios")
-var flash=require("connect-flash");
-var MemoryStore=require("memorystore")(session)
+var axios = require("axios")
+var flash = require("connect-flash");
+var MemoryStore = require("memorystore")(session)
 
 
 
@@ -28,13 +28,13 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(cookieParser('secret'));
 app.use(session({
-    cookie: { maxAge: 86400000},
-    store: new MemoryStore({
-      checkPeriod:86400000
-    }),
-    saveUninitialized: true,
-    resave: 'true',
-    secret: 'secret'
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000
+  }),
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
 }));
 app.use(flash());
 
@@ -50,6 +50,32 @@ var hbs = exphbs.create({
       moment.tz.add("America/Monterrey|LMT CST CDT|6F.g 60 50|0121212121212121212121212121212121212121212121212121212121212121212121212121212121212121|-1UQG0 2FjC0 1nX0 i6p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 1fB0 WL0 1fB0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0|41e5")
       var fechaCreacion = moment(property).tz("America/Monterrey").format("DD/MM/YYYY hh:mm:ss a");
       return fechaCreacion;
+    },
+    ifCond: function (v1, operator, v2, options) {
+      switch (operator) {
+        case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+          return options.inverse(this);
+      }
     }
   }
 })
@@ -62,7 +88,7 @@ app.engine(
 app.set("view engine", "handlebars");
 
 //*Global Vars
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   // if there's a flash message in the session request, make it available in the response, then delete it
   res.locals.sessionFlash = req.session.sessionFlash;
   delete req.session.sessionFlash;
@@ -70,6 +96,17 @@ app.use(function(req, res, next){
   res.locals.error = req.flash('error');
   res.locals.success = req.flash('success');
   res.locals.user = req.user || null;
+  if (req.user) {
+    if (req.user.role === "admin") {
+      res.locals.admin = "admin"
+    }
+    if (req.user.role === "produccion") {
+      res.locals.produccion = "produccion"
+    }
+    if (req.user.role === "inspector") {
+      res.locals.inspector = "inspector"
+    }
+  }
   next();
 });
 
