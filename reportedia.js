7fa9b50f1caf47a105d353bf73ae967d6a67f3a5
Gustavo Canales <gustavo.canales@me.com>
2019-04-21 14:58:38 -0500

    Horario de verano automatico

--- reportedia.js
+++ reportedia.js
@@ -1,35 +1,51 @@
 const moment = require('moment-timezone');
-const axios=require("axios");
+const axios = require("axios");
 
-let horainicial = moment().format("YYYY-MM-DD") + " 12:00:00"
-let horainicialx = moment(horainicial).format("X")
-let horafinal = moment().format("YYYY-MM-DD") + " 20:00:00"
-let horafinalx = moment(horafinal).format("X")
-let dia=moment(horafinal).format("dddd");
-//console.log(dia)
 
-if(dia!="Sunday"){
-reporte();
-//console.log("Hello World")
+/**Si es horario de verna */
+if (moment().isDST()) {
+  //console.log("Es horario de verano")
+  var horainicial = moment().format("YYYY-MM-DD") + " 12:00:00"
+  var horainicialx = moment(horainicial).format("X")
+  var horafinal = moment().format("YYYY-MM-DD") + " 20:00:00"
+  var horafinalx = moment(horafinal).format("X")
+  var dia = moment(horafinal).format("dddd");
+  //console.log(dia)
+}
+else {
+  //console.log("No es horario de verano")
+  var horainicial = moment().format("YYYY-MM-DD") + " 13:00:00"
+  var horainicialx = moment(horainicial).format("X")
+  var horafinal = moment().format("YYYY-MM-DD") + " 21:00:00"
+  var horafinalx = moment(horafinal).format("X")
+  var dia = moment(horafinal).format("dddd");
+  //console.log(dia)
+}
+
+
+
+if (dia === "Sunday") {
+  reporte();
+  //console.log("Hello World")
 }
 
 function reporte() {
   console.log("https://shielded-stream-29921.herokuapp.com/produccionhora/" + horainicialx + "/" + horafinalx)
   axios.get("https://shielded-stream-29921.herokuapp.com/produccionhora/" + horainicialx + "/" + horafinalx)
-  .then(data => {
+    .then(data => {
       console.log(data.data.count)
-      axios.post("https://shielded-stream-29921.herokuapp.com/reporte",{
-        piezasProducidas:data.data.count,
-        turno:"dia"
+      axios.post("https://shielded-stream-29921.herokuapp.com/reporte", {
+        piezasProducidas: data.data.count,
+        turno: "dia"
       })
-      .then(function(response){
-        //console.log(response)
-      })
-      .catch(function(err){
-        console.log(err)
-      })
+        .then(function (response) {
+          //console.log(response)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
     }).catch(function (err) {
       console.log(err)
     })
-  
+
 }
--- reportetarde.js
+++ reportetarde.js
@@ -1,33 +1,44 @@
 const moment = require('moment-timezone');
-const axios=require("axios");
+const axios = require("axios");
 
-let horainicial = moment().subtract(1,"day").format("YYYY-MM-DD") + " 20:00:00"
-let horainicialx = moment(horainicial).format("X")
-let horafinal = moment().add(0,"day").format("YYYY-MM-DD") + " 04:00:00"
-let horafinalx = moment(horafinal).format("X")
-let dia=moment(horafinal).format("dddd")
 
-if (dia!="Monday"){
-reporte();
+if (moment().isDST()) {
+  var horainicial = moment().subtract(1, "day").format("YYYY-MM-DD") + " 20:00:00"
+  var horainicialx = moment(horainicial).format("X")
+  var horafinal = moment().add(0, "day").format("YYYY-MM-DD") + " 04:00:00"
+  var horafinalx = moment(horafinal).format("X")
+  var dia = moment(horafinal).format("dddd")
+}
+else {
+  var horainicial = moment().subtract(1, "day").format("YYYY-MM-DD") + " 21:00:00"
+  var horainicialx = moment(horainicial).format("X")
+  var horafinal = moment().add(0, "day").format("YYYY-MM-DD") + " 05:00:00"
+  var horafinalx = moment(horafinal).format("X")
+  var dia = moment(horafinal).format("dddd")
+
+}
+
+if (dia != "Monday") {
+  reporte();
 }
 
 function reporte() {
   console.log("https://shielded-stream-29921.herokuapp.com/produccionhora/" + horainicialx + "/" + horafinalx)
   axios.get("https://shielded-stream-29921.herokuapp.com/produccionhora/" + horainicialx + "/" + horafinalx)
-  .then(data => {
+    .then(data => {
       console.log(data.data.count)
-      axios.post("https://shielded-stream-29921.herokuapp.com/reporte",{
-        piezasProducidas:data.data.count,
-        turno:"tarde"
+      axios.post("https://shielded-stream-29921.herokuapp.com/reporte", {
+        piezasProducidas: data.data.count,
+        turno: "tarde"
       })
-      .then(function(response){
-        //console.log(response)
-      })
-      .catch(function(err){
-        console.log(err)
-      })
+        .then(function (response) {
+          //console.log(response)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
     }).catch(function (err) {
       console.log(err)
     })
-  
+
 }
