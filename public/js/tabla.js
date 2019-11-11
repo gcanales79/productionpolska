b60e7b41660926e42fe3947315ae0d2268bc9af8
gcanales <gustavo.canales@me.com>
2019-11-11 18:38:37 +0100

    Improve table and correct error

--- public/js/tabla.js
+++ public/js/tabla.js
@@ -4,10 +4,24 @@ $("#buscarRegistros").on("click", function (event) {
     url = "/tabla/" + numeroDeregistros;
     console.log("El url es " + url);
     window.location.href = url;
-   
+
 
 
 });
 
-$("#tablaConsulta").DataTable();
+$("#tablaConsulta").DataTable({
+    "language": {
+        "lengthMenu": "Mostrar _MENU_ registros por pagina",
+        "zeroRecords": "No hay datos que mostrar",
+        "info": "Mostrando del _START_ a _END_ de _TOTAL_ registros",
+        "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Previo"
+        },
+        "search": "Buscar:",
+    }
+});
 
