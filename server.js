ac227f2267b33563c753f4f3d346c335fb5178c3
Gustavo Canales <gustavo.canales@me.com>
2019-12-02 00:12:16 +0100

    cron

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 //Cron Job
-new CronJob('11 00 * * *', function() {
+new CronJob('00 08 * * *', function() {
   console.log("starting cron");
   if(shell.exec("node cron.js").code !==0){
     console.log("Something went wrong")
