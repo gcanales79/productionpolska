d578566b068902376a5c87b4af836fb42df7ebeb
Gus <gustavo.canales@me.com>
2020-06-16 14:55:14 +0200

    fix fecha

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -143,7 +143,7 @@ module.exports = function (app) {
         stf4_hr16: req.body.stf4_hr16,
         turno: req.body.shift,
         dia: moment(req.body.date, "DD-MM-YYYY").subtract(1, "day").day(),
-        fecha: moment(req.body.fecha).subtract(1, "day").format("YYYY-MM-DD"),
+        fecha: moment(req.body.date).subtract(1, "day").format("YYYY-MM-DD"),
       }).then(function (dbPolska2) {
         res.json(dbPolska2)
       })
@@ -169,7 +169,7 @@ module.exports = function (app) {
         stf4_hr16: req.body.stf4_hr16,
         turno: req.body.shift,
         dia: moment(req.body.date, "DD-MM-YYYY").day(),
-        fecha: moment(req.body.fecha).format("DD-MM-YYYY"),
+        fecha: moment(req.body.date).format("DD-MM-YYYY"),
       }).then(function (dbPolska2) {
         res.json(dbPolska2)
       })
