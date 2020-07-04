868e418ab80536b441255dfb2ead99db6b08c494
Gus <gustavo.canales@me.com>
2020-07-04 15:09:50 +0200

    fix the date in shift 1 and 2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -169,7 +169,7 @@ module.exports = function (app) {
         stf4_br10: req.body.stf4_br10,
         stf4_hr16: req.body.stf4_hr16,
         turno: req.body.shift,
-        dia: moment(req.body.date, "DD-MM-YYYY").day(),
+        dia: moment(req.body.date, "YYYY-MM-DD").day(),
         fecha: moment(req.body.date,"YYYY-MM-DD").format("YYYY-MM-DD"),
       }).then(function (dbPolska2) {
         res.json(dbPolska2)
