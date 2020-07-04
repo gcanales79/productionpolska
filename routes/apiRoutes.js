43a7671a3565495ae9d39169aeadbb1feea0f4f3
Gus <gustavo.canales@me.com>
2020-07-04 12:57:13 +0200

    change date formatr for shift 1 and 2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -170,7 +170,7 @@ module.exports = function (app) {
         stf4_hr16: req.body.stf4_hr16,
         turno: req.body.shift,
         dia: moment(req.body.date, "DD-MM-YYYY").day(),
-        fecha: moment(req.body.date,"DD-MM-YYYY").format("YYYY-MM-DD"),
+        fecha: moment(req.body.date,"YYYY-MM-DD").format("YYYY-MM-DD"),
       }).then(function (dbPolska2) {
         res.json(dbPolska2)
       })
